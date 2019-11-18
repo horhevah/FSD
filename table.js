595baa1b7686d4a30b1424e2f9ad96998653eb36
horhevah <56997734+horhevah@users.noreply.github.com>
2019-11-18 16:04:51 +0300

    доработал календарь

--- table.js
+++ table.js
@@ -1,22 +1,44 @@
 'use strict';
-var tab = document.querySelector('.form-3 table');
-console.log(tab);
-var tr=[];
-var td=[];
-var day=1;
-var text;
-for (var i = 0; i<5; i++){
-tr[i] = document.createElement('tr');
-tr[i].className='tr tr-'+i;
-tab.appendChild(tr[i]);
-console.log(tr[i]);
+var addDiv = function(parent,cssclass){
+  var elem = document.createElement('div');
+  elem.className=cssclass;
+  parent.appendChild(elem);
+  return elem;
+}
+var calendar = document.querySelector('.form-3 .calendar');
+var title = addDiv(calendar,'calendar-title')
+var day=[];
+var week = [];
+var startday=4;
+var maxdate=31;
+var date=1;
+var date_in;
+var weekday = ['пн','вт','ср','чт','пт','сб','вс']
+for (var i = 0; i < 7; i++) {
+  var day_of_week = addDiv(title,'day-of-week');
+day_of_week.textContent = weekday[i];
+}
 
+for (var i = 0; i < 5; i++) {
+week[i] = addDiv(calendar,'week week-'+i);
+console.log(week[i],i);
 for (var j = 0; j < 7; j++) {
-  td[j] = document.createElement('td');
-  td[j].className='td td-'+day;
-  text=document.createTextNode(day);
-  td[j].appendChild(text);
-  tr[i].appendChild(td[j]);
-  day++;
+console.log(j);
+day[j] = addDiv(week[i],'day day-'+j);
+if (date<startday){
+ date_in=maxdate-startday+date;
+ day[j].classList.add('grey');
+}
+else{
+  if(date_in>=maxdate){
+    date_in=date-maxdate-startday+1;
+    day[j].classList.add('grey');
+  }
+  else{
+    date_in=date-startday+1;
+  }
+}
+day[j].textContent = date_in;
+date++;
 }
 }
