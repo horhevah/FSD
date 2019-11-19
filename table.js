b657cfc2a257861e6d32aa80165f784c6b031d57
horhevah <56997734+horhevah@users.noreply.github.com>
2019-11-19 17:59:42 +0300

    Улучшение календаря

--- table.js
+++ table.js
@@ -13,6 +13,8 @@ var startday=4;
 var maxdate=31;
 var date=1;
 var date_in;
+var n=0;
+var vacant=[];
 var weekday = ['пн','вт','ср','чт','пт','сб','вс']
 for (var i = 0; i < 7; i++) {
   var day_of_week = addDiv(title,'day-of-week');
@@ -21,10 +23,10 @@ day_of_week.textContent = weekday[i];
 
 for (var i = 0; i < 5; i++) {
 week[i] = addDiv(calendar,'week week-'+i);
-console.log(week[i],i);
 for (var j = 0; j < 7; j++) {
-console.log(j);
-day[j] = addDiv(week[i],'day day-'+j);
+day[j] = addDiv(week[i],'day day-'+(i*7+j+1));
+
+
 if (date<startday){
  date_in=maxdate-startday+date;
  day[j].classList.add('grey');
@@ -40,5 +42,42 @@ else{
 }
 day[j].textContent = date_in;
 date++;
+if (!day[j].classList.contains('grey')) {
+ day[j].addEventListener('click',
+  function(evt){
+   if(!evt.currentTarget.classList.contains('checked')&&n<2){
+    evt.currentTarget.classList.add('checked');
+    vacant[n]=+evt.currentTarget.textContent;
+    console.log(vacant[n], day[j]);
+    console.log(n);
+    /*if(vacant[1]){
+      if(vacant[0]<vacant[1]){
+        var c = vacant[0];
+        vacant[0]=vacant[1];
+        vacant[1]=c;
+      }*/
+      for(var k=vacant[0]+1; k<vacant[1]; k++){
+      var test = document.querySelector('.day-'+(k+startday-1));
+      console.log(test);
+      test.classList.add('mchecked');
+      }
+    
+    n++;
+   }
+     else if (evt.currentTarget.classList.contains('checked')){
+    evt.currentTarget.classList.remove('checked');
+    n--;
+    }
+  })
+
 }
 }
+}
+
+
+
+//var days = document.querySelectorAll('.day');
+//for (var i = 0; i < 35; i++) {
+  //console.log(days[i],i);
+
+//}
