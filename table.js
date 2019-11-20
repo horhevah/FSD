2535ce744ea5f15a6d37931e65e646369274e45b
horhevah <56997734+horhevah@users.noreply.github.com>
2019-11-20 13:05:43 +0300

    Сделал внешне кнопку и добил выбор даты календаря.

--- table.js
+++ table.js
@@ -14,6 +14,7 @@ var maxdate=31;
 var date=1;
 var date_in;
 var n=0;
+var t=0;
 var vacant=[];
 var weekday = ['пн','вт','ср','чт','пт','сб','вс']
 for (var i = 0; i < 7; i++) {
@@ -26,20 +27,18 @@ week[i] = addDiv(calendar,'week week-'+i);
 for (var j = 0; j < 7; j++) {
 day[j] = addDiv(week[i],'day day-'+(i*7+j+1));
 
-
 if (date<startday){
  date_in=maxdate-startday+date;
  day[j].classList.add('grey');
 }
-else{
-  if(date_in>=maxdate){
+else if(date_in>=maxdate){
     date_in=date-maxdate-startday+1;
     day[j].classList.add('grey');
+
   }
   else{
     date_in=date-startday+1;
   }
-}
 day[j].textContent = date_in;
 date++;
 if (!day[j].classList.contains('grey')) {
@@ -47,26 +46,36 @@ if (!day[j].classList.contains('grey')) {
   function(evt){
    if(!evt.currentTarget.classList.contains('checked')&&n<2){
     evt.currentTarget.classList.add('checked');
-    vacant[n]=+evt.currentTarget.textContent;
-    console.log(vacant[n], day[j]);
-    console.log(n);
-    /*if(vacant[1]){
-      if(vacant[0]<vacant[1]){
-        var c = vacant[0];
-        vacant[0]=vacant[1];
-        vacant[1]=c;
-      }*/
-      for(var k=vacant[0]+1; k<vacant[1]; k++){
-      var test = document.querySelector('.day-'+(k+startday-1));
-      console.log(test);
-      test.classList.add('mchecked');
+    vacant[t]=+evt.currentTarget.textContent;
+        if(vacant[0]>vacant[1]){
+        var c = vacant[1];
+        vacant[1]=vacant[0];
+        vacant[0]=c;
       }
-    
+      for(var k=vacant[0]+1; k<vacant[1]; k++){
+      document.querySelector('.day-'+(k+startday-1)).classList.add('mchecked');
+      document.querySelector('.day-'+(vacant[0]+startday-1)).classList.add('firstchecked');
+      document.querySelector('.day-'+(vacant[1]+startday-1)).classList.add('secondchecked');
+      }
     n++;
+    t++;
+    console.log(n,t,vacant[0],vacant[1], evt.currentTarget);
    }
      else if (evt.currentTarget.classList.contains('checked')){
-    evt.currentTarget.classList.remove('checked');
+     evt.currentTarget.classList.remove('checked');
+      for(var k=vacant[0]+1; k<vacant[1]; k++){
+
+      document.querySelector('.day-'+(k+startday-1)).classList.remove('mchecked');
+      document.querySelector('.day-'+(vacant[0]+startday-1)).classList.remove('firstchecked');
+      document.querySelector('.day-'+(vacant[1]+startday-1)).classList.remove('secondchecked');
+    }
+    for(var z=0; z<2; z++){
+    if(vacant[z] == +evt.currentTarget.textContent){
+      vacant[z]=undefined;
+      if (n==1&&z==1){ t=0 }else{t=z};
+    }}
     n--;
+    console.log(n,t,vacant[0],vacant[1], evt.currentTarget);
     }
   })
 
