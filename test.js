f7350450bba1560c78b32f395ba0561a1fe89b29
horhevah <56997734+horhevah@users.noreply.github.com>
2019-11-28 12:07:07 +0300

    добавил в слайдер значение

--- test.js
+++ test.js
@@ -250,10 +250,12 @@ var currentCoord;
 var startD;
 var d = 0;
 var max = 320;
+
 var pointStartTranslate = function(evt){
   console.log(0);
   startCoord = evt.clientX;
   startD = d;
+  point_value.classList.remove('hidden');
   console.log(startCoord,evt.clientX);
     document.addEventListener('mousemove',pointMove);
     document.addEventListener('mouseup',pointMoveStop);
@@ -264,13 +266,19 @@ var pointMove = function(evt){
   d = startD + currentCoord - startCoord;
   if(d>=0&&d<max){
     point.style.left = d + 'px';
+    point_value.style.left = d + 'px';
+    point_value.textContent = d;
     console.log(startCoord,evt.clientX,currentCoord,point.style.left,d);
   }else if(d<=0){
     d=0;
     point.style.left = d + 'px';
+    point_value.style.left = d + 'px';
+    point_value.textContent = d;
   }else{
     d=max;
     point.style.left = d + 'px';
+    point_value.style.left = d + 'px';
+    point_value.textContent = d;
   }
 
   }
@@ -279,8 +287,10 @@ var pointMoveStop = function(evt) {
   console.log(1);
   document.removeEventListener('mousemove',pointMove);
   document.removeEventListener('mouseup',pointMoveStop);
+  point_value.classList.add('hidden');
 }
 
 var point = document.querySelector('.point');
+var point_value = document.querySelector('.point_value');
 
 point.addEventListener('mousedown',pointStartTranslate);
